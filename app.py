02f1ea75c091f7d8fc524c65cf668f3e709b857b
luizhenriquesccp <147549836+luizhenriquesccp@users.noreply.github.com>
2025-09-30 21:25:24 -0300

    Ajuste para exibição correta

--- app.py
+++ app.py
@@ -24,3 +24,19 @@ def lista_exercicios():
     cnx.close()
 
     return render_template("exercicio.html", banco=resultado)
+
+@app.route("/exercicio/<int:id>")
+def exibir_exercicio(id):
+    cnx = connection.MySQLConnection(
+        user="root",
+        password="labinfo",
+        database="respiraai", 
+        host="127.0.0.1"
+    )
+    cursor = cnx.cursor(dictionary=True)
+    sql = "SELECT nome, descriçao as descricao, duraçao as duracao FROM exercicio WHERE id = %s"
+    cursor.execute(sql, (id,))
+    resultado = cursor.fetchone()
+    cursor.close()
+    cnx.close()
+    return render_template("ex.html", exercicio=resultado)
